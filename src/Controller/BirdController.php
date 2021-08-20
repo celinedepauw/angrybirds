8664b9ec4e7301ee5cb8657d0bcf1450c7187fbe
celin <celin@localhost>
2021-08-20 12:09:51 +0200

    get datas in BirdController

--- src/Controller/BirdController.php
+++ src/Controller/BirdController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
+use App\Model\BirdModel;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BirdController
 {
+    /**
+     * @Route("/", name="home")
+     */
     public function home()
     {
+        // we get the birds list
+        $birdModel = new BirdModel();
+        $birds = $birdModel->getBirds();
+
+        dump($birds);
         return new Response('Home page');
     }
 }
