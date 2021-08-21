5dfaf5b005f69238ae7bbd960249017bbe5873ce
celin <celin@localhost>
2021-08-21 11:18:17 +0200

    integrate html and link css

--- src/Controller/BirdController.php
+++ src/Controller/BirdController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Model\BirdModel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BirdController extends AbstractController
